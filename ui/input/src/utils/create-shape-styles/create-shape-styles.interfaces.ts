9ceb8b702877a400cd06503a12afbda563aea975
Viktoria Dragun <aliceinapple99@gmail.com>
2024-06-20 22:35:03 +0300

    feat: add create-shape-styles util

---
+++ ui/input/src/utils/create-shape-styles/create-shape-styles.interfaces.ts
@@ -0,0 +1,11 @@
+export interface InputShapeStyles {
+  size: number
+  borderWidth?: number | string
+  fontFamily?: string
+  fontSize?: number
+  fontWeight?: string | number
+  rounding?: number
+  paddingLeft?: number
+  paddingRight?: number
+  paddingRatio?: number
+}
