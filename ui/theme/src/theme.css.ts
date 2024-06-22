c53a0e9f4e14686bb8a4f9963c5f7f5ef2a2e17b
Dmitry <dimafomin02@gmail.com>
2024-06-22 06:05:25 +0300

    feat: added new theme vars

--- ui/theme/src/theme.css.ts
+++ ui/theme/src/theme.css.ts
@@ -2,20 +2,24 @@ import './global.css'
 
 import { createGlobalTheme } from '@vanilla-extract/css'
 
+import { borders }           from './theme'
 import { colors }            from './theme'
-import { radii }             from './theme'
+import { fonts }             from './theme'
 import { fontSizes }         from './theme'
 import { fontWeights }       from './theme'
-import { fonts }             from './theme'
 import { lineHeights }       from './theme'
+import { radii }             from './theme'
+import { shadows }           from './theme'
 import { space }             from './theme'
 
 export const vars = createGlobalTheme(':root', {
+  borders,
   colors,
-  radii,
-  space,
-  fontSizes,
   fonts,
-  lineHeights,
+  fontSizes,
   fontWeights,
+  lineHeights,
+  radii,
+  shadows,
+  space,
 })
