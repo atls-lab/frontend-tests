9940ab471389c004c41343a5675d4ce7bff4bd43
Viktoria Dragun <aliceinapple99@gmail.com>
2024-06-26 00:28:41 +0300

    feat: add menu item component

---
+++ ui/select/src/item/item.component.tsx
@@ -0,0 +1,18 @@
+import { style }                    from '@vanilla-extract/css'
+
+import React                       from 'react'
+
+import { PropsWithChildren } from 'react'
+
+import { ItemProps }                from './item.interfaces'
+import { baseItemStyles }           from './item.styles.css'
+
+const highlightedMenuItem = style({
+  backgroundColor: 'aqua',
+})
+
+export const MenuItem: React.FC<PropsWithChildren<ItemProps>> = ({ highlighted, children }) => {
+  const classNames = style([baseItemStyles, highlighted ? highlightedMenuItem : 'aqua'])
+
+  return <li className={classNames}>{children}</li>
+}
---
+++ ui/select/src/item/item.styles.css.ts
@@ -0,0 +1,10 @@
+import { style } from '@vanilla-extract/css'
+
+export const baseItemStyles = style({
+  display: 'flex',
+  boxSizing: 'border-box',
+  cursor: 'pointer',
+  width: '100%',
+  hyphens: 'auto',
+  alignItems: 'center',
+})
