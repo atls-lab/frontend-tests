74a6fa3dfb923c2ce59e796c3f8d4fb9f17159c7
Dmitry <dimafomin02@gmail.com>
2024-06-27 01:53:55 +0300

    chore: changed vanilla-extract plugin in next config

--- app-router/entrypoints/renderer/next.config.js
+++ app-router/entrypoints/renderer/next.config.js
@@ -2,24 +2,23 @@ import { join } from 'path'
 import { dirname } from 'path'
 
 import { fileURLToPath } from 'url'
-import { VanillaExtractPlugin } from '@vanilla-extract/webpack-plugin'
+
+import { createVanillaExtractPlugin } from '@vanilla-extract/next-plugin'
+
+const withVanillaExtract = createVanillaExtractPlugin()
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
 /** @type {import('next').NextConfig} */
-export default {
+export default withVanillaExtract({
   experimental: {
     externalDir: true,
     outputFileTracingRoot: join(__dirname, './'),
     esmExternals: 'loose',
   },
-  compiler: {
-    styledComponents: true,
-  },
   webpack: (webpackConfig, { webpack }) => {
     webpackConfig.plugins.push(
-      new VanillaExtractPlugin(),
       new webpack.NormalModuleReplacementPlugin(new RegExp(/\.js$/), function (
         /** @type {{ request: string }} */
         resource
@@ -30,4 +29,4 @@ export default {
     return webpackConfig
   },
   output: 'standalone',
-}
+})
