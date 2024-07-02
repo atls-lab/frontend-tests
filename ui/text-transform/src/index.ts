0330c63b312f8a4dd29c9f21c45d2af92d6bb843
Dmitry <dimafomin02@gmail.com>
2024-07-02 18:43:22 +0300

    feat: added text-transform component

---
+++ ui/text-transform/src/index.ts
@@ -0,0 +1,2 @@
+export * from './text-transform.component'
+export * from './text-transform.interfaces'
---
+++ ui/text-transform/src/text-transform.interfaces.ts
@@ -0,0 +1,10 @@
+export interface TextTransformProps {
+  children: string
+  upperCase?: boolean
+  lowerCase?: boolean
+  firstLetter?: boolean
+  lastLetter?: boolean
+  replace?: [string, string]
+  substr?: number | [number, number]
+  hideAfter?: number
+}
