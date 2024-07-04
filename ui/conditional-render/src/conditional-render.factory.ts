7bb90ecd43de15b39de577a722e5e620e0ede57e
Dmitry <dimafomin02@gmail.com>
2024-07-04 03:24:35 +0300

    feat: added @ui/conditional-render

---
+++ ui/conditional-render/src/conditional-render.factory.ts
@@ -0,0 +1,23 @@
+import { createElement } from 'react'
+
+export type ConditionFunction = (props: any) => boolean
+export type Condition = ConditionFunction | string
+
+export const ConditionalRender = (tag: string = 'div', condition: Condition = 'children') => {
+  if (typeof condition === 'function') {
+    // eslint-disable-next-line
+    return (props) => {
+      if (!condition(props)) return null
+
+      return createElement(tag, props)
+    }
+  }
+
+  // eslint-disable-next-line
+  return (props) => {
+    // eslint-disable-next-line react/destructuring-assignment
+    if (!props[condition]) return null
+
+    return createElement(tag, props)
+  }
+}
