cae3b2b9a42758206d56a3d3aca360ca35df9137
Viktoria Dragun <aliceinapple99@gmail.com>
2024-07-05 00:15:57 +0300

    feat: add label to checkbox

---
+++ ui/checkbox/src/label/position/position.styles.css.ts
@@ -0,0 +1,10 @@
+import { style } from '@vanilla-extract/css'
+
+export const labelPositionStyles = {
+  label: style({
+    marginRight: '0',
+    marginLeft: '0',
+    marginBottom: '0',
+    marginTop: '0',
+  }),
+}
---
+++ ui/checkbox/src/label/shape/shape.styles.css.ts
@@ -0,0 +1,9 @@
+import { style } from '@vanilla-extract/css'
+
+export const labelShapeStyles = {
+  label: style({
+    fontSize: 16,
+    fontWeight: 'normal',
+    fontFamily: 'Arial, sans-serif',
+  }),
+}
