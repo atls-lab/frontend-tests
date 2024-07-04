961401bf874095386be2b7023528c278649cd0f7
Viktoria Dragun <aliceinapple99@gmail.com>
2024-07-05 00:45:34 +0300

    feat: checkbox component

---
+++ ui/checkbox/src/checkbox.component.tsx
@@ -0,0 +1,20 @@
+import React             from 'react'
+
+import { CheckboxProps }  from './checkbox.interfaces'
+import { checkboxStyles } from './checkbox.styles.css'
+
+const Checkbox: React.FC<CheckboxProps> = ({ checked, onChange, disabled = false }) => {
+  return (
+    <label className={checkboxStyles}>
+      <input
+        type='checkbox'
+        checked={checked}
+        onChange={(e) => onChange(e.target.checked)}
+        disabled={disabled}
+      />
+      <span>Checkbox Label</span>
+    </label>
+  )
+}
+
+export default Checkbox
---
+++ ui/checkbox/src/checkbox.styles.css.ts
@@ -0,0 +1,13 @@
+import { style }                                                               from '@vanilla-extract/css'
+
+import { checkBaseStyles }                                                      from './check'
+
+import { createCheckAppearanceStyles }                         from './check'
+
+import { createCheckShapeStyles } from './check'
+
+export const checkboxStyles = style([
+  checkBaseStyles,
+  createCheckAppearanceStyles({}),
+  createCheckShapeStyles({ size: 16 }),
+])
